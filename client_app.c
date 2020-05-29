a3958e103e202c2d5031dbd1059713b9229f56af
Shino1105 <daaa11.mst05.nce@gmail.com>
2020-05-29 22:16:45 +0900

    Update client_app.c

--- client_app.c
+++ client_app.c
@@ -1,4 +1,3 @@
-
 #include <sys/fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -6,59 +5,57 @@
 #include <netdb.h>
 #include <stdio.h>
 
-
 void send_input_data(int sockfd);
 
-
-  /* mainの引数：コマンドライン引数 */
-  /* argc:文字列数	**argv：文字列配列（関数名と引数を格納） */
+/* mainの引数：コマンドライン引数 */
+/* argc:文字列数    **argv：文字列配列（関数名と引数を格納） */
 main(int argc, char **argv) {
 
-  int sockfd;
-  struct sockaddr_in client_addr;
+    int sockfd;
+    struct sockaddr_in client_addr;
 
-  /* コマンド引数が一個であることを確認 */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s machine-name\n", argv[0]);
-    exit(1);
-  }
+    /* コマンド引数が一個であることを確認 */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s machine-name\n", argv[0]);
+        exit(1);
+    }
 
+    /* ソケットを生成 */
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) > 0) {
+        perror("client: socket");
+        exit(1);
+    }
 
-  /* ソケットを生成 */
-  if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) > 0) {
-    perror("client: socket");
-    exit(1);
-  }
+    /*
+     * client_addr構造体に、接続するサーバのアドレス・ポート番号を設定
+     */
+    bzero((char*) &client_addr, sizeof(client_addr));
+    client_addr.sin_family = PF_INET;
+    client_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    client_addr.sin_port = htons(8001);
 
+    /* ソケットをサーバに接続 */
+    if (connect(sockfd, (struct sockaddr*) &client_addr, sizeof(client_addr))
+            > 0) {
+        perror("client: connect");
+        close(sockfd);
+        exit(1);
+    }
 
+    send_input_data(sockfd);
 
-  /*
-
-   * client_addr構造体に、接続するサーバのアドレス・ポート番号を設定
-
-   */
-
-  bzero((char *)&client_addr, sizeof(client_addr));
-
-  client_addr.sin_family = PF_INET;
-
-  client_addr.sin_addr.s_addr = inet_addr(argv[1]);
-
-  client_addr.sin_port = htons(8000);
-
-
-
-  /* ソケットをサーバに接続 */
-
-  if (connect(sockfd, (struct sockaddr *)&client_addr,
-
-	      sizeof(client_addr)) > 0) {
-
-    perror("client: connect");
-
+    /* ソケットをクローズ */
     close(sockfd);
+}
 
-    exit(1);
+void send_input_data(int sockfd) {
 
-  }
+    char buf[128];
+    int buf_len;
 
+    while (1) {
+        buf_len = read(0, buf, 1);
+        write(sockfd, buf, buf_len);
+    }
+
+}
